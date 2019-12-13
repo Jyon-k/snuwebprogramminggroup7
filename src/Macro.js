f99065a8396d1e0230f5f0c08c45bc3c8245ab3b
Jinyon Jyon Kim <jykim@kdb.snu.ac.kr>
2019-12-13 19:54:17 +0900

    [JY] checkpoint to pull

--- src/Macro.js
+++ src/Macro.js
@@ -3,4 +3,33 @@
  *
  * Automatically check if target classes get seats.
  */
+/* eslint-disable react/jsx-filename-extension */
+
 import React from 'react';
+
+
+const watchClasses = (/*{getHeaders}*/) => {
+//    //const headers = getHeaders;
+//
+//    const url = "https://sugang.snu.ac.kr";
+//
+//    fetch(`${url}/sugang/cc/cc210.action`, {
+//            method: "GET",
+//            mode: 'cors',
+//            headers: {
+//                'upgrade-insecure-requests': "1",
+//                'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+//                'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+//                'referer': "http://sugang.snu.ac.kr/sugang/cc/cc210.action",
+//                'accept-encoding': "gzip, deflate",
+//                'accept-language': "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+//                'cookie': "__utma=134997939.1340164299.1472112698.1504232470.1512035733.4; _ga=GA1.3.1340164299.1472112698; WMONID=z32M4ZJHE7Y; enter=Y; JSESSIONID=nILc0RZ2FglItJhfe3DdWRLEpFUY7Pb8XOcrd208tvPCPt1pU5UCi48maH1ORBWj.giants1_servlet_engine2",
+//                'cache-control': "no-cache",
+//                'postman-token': "efa451cf-a6c2-5374-c660-42f69dd5f82a",
+//            }
+//        })
+//    .then((res) => console.log(res));
+//
+}
+
+export default watchClasses;
