148fefcd11acef199d981bb804b2d8397c3101bc
dydtjrsj21 <44419206+dydtjrsj21@users.noreply.github.com>
2019-12-19 17:42:42 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Auth from './Auth'
 import Macro from './Macro';
 import superagent from 'superagent'
+import image from './number.jpg'
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   const [id, setId] = useState(null);
   const [psw, setPsw] = useState(null);
   const [cb, setCb] = useState(null);
-  // const [ocr, setOcr] = useState(null);
+  const [ocr, setOcr] = useState(null);
 
   const onLogin = (e) => {
     e.preventDefault();
@@ -33,37 +34,37 @@ function App() {
 
   const onRegister = (e) => {
     e.preventDefault();
-
-    superagent
-          .post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDCygc6BPZ0Fj3ApJEWHgLeAqZlawWm5Bw')
-          .send({
-                  'requests': [
-                      {
-                      'image': {
-                          'source': {
-                          'imageUri': 'http://sugang.snu.ac.kr/sugang/ca/number.action?v=0.28415202633175474' //이미지 소스
-                          }
-                      },
-                      'features': [
-                          {
-                          'type': 'DOCUMENT_TEXT_DETECTION'
-                          }
-                      ]
-                      }
-                  ]
-                  }) // sends a JSON post body
-      .set('accept', 'json')
-          .then((response) => eval("("+response.text+")"))
-          .then((res) => { 
-            atest.register(cb, parseInt(res.responses[0].textAnnotations[0].description))
-            .then((result) => {
-              console.log(result);
-            })  
-          })
-          .catch((err) => console.error(err));
-
-
-    
+    atest.register(cb, ocr)
+    .then((result) => {
+      console.log(result);
+//     superagent
+//     .post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDCygc6BPZ0Fj3ApJEWHgLeAqZlawWm5Bw')
+//     .send({
+//             'requests': [
+//                 {
+//                 'image': {
+//                     'source': {
+//                     'imageUri': 'http://sugang.snu.ac.kr/sugang/ca/number.action?v=0.28415202633175474' //이미지 소스
+//                     }
+//                 },
+//                 'features': [
+//                     {
+//                     'type': 'DOCUMENT_TEXT_DETECTION'
+//                     }
+//                 ]
+//                 }
+//             ]
+//             }) // sends a JSON post body
+//     .set('accept', 'json')
+//     .then((response) => eval("("+response.text+")"))
+//     .then((res) => { 
+//       atest.register(cb, parseInt(res.responses[0].textAnnotations[0].description))
+//       .then((result) => {
+//         console.log(result);
+//       })  
+//     })
+//     .catch((err) => console.error(err));
+    })   
   }
 
   //var test = new Macro();
@@ -80,11 +81,11 @@ function App() {
       <form onSubmit={onRegister}>
         <input type="text" placeholder="cb"
           onChange={(e) => setCb(e.target.value)} />
-        {/* <input type="text" placeholder="ocr"
-          onChange={(e) => setOcr(e.target.value)} /> */}
+        <input type="text" placeholder="ocr"
+          onChange={(e) => setOcr(e.target.value)} />
         <input type="submit" value="Register" />
       </form>
-
+      <img src={image}></img>
     </div>
   );
 }
